421cd560bee28d98de041f5dae3c10bd5a11d90a
travisdock <travisdock@gmail.com>
2018-05-29 18:52:42 -0400

    add views and controller routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,30 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render "new"
+    end
+  end
+
+
+  private
+  def event_params
+    params.require(:event).permit(:date, :time, :where)
+  end
+
 end
