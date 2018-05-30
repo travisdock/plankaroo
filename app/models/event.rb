3aab59a70cab0d894693632fa68dd072da337880
snkoski <shawnnkoski@gmail.com>
2018-05-30 11:18:36 -0400

    add plank view, refactor event methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
   has_many :planks
   has_many :users, through: :planks
+
+  def display_date
+    self.date.strftime("%A, %B %-d")
+  end
+
+  def display_time
+    self.time.strftime("%I:%M %p")
+  end
+
+
 end
