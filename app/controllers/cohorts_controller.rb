9d7390e3b657615a5c513d1c2a0fc398b63f1a74
snkoski <shawnnkoski@gmail.com>
2018-05-30 09:35:05 -0400

    cohort index, new, show, create controllers. cohort new and show views

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,2 +1,30 @@
 class CohortsController < ApplicationController
+
+  def new
+    @cohort = Cohort.new
+  end
+
+  def create
+    @cohort = Cohort.new(cohort_params)
+
+    if @cohort.save
+      redirect_to @cohort
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @cohorts = Cohort.all
+  end
+
+  def show
+    @cohort = Cohort.find(params[:id])
+  end
+
+  private
+
+  def cohort_params
+    params.require(:cohort).permit(:name, :motto)
+  end
 end
